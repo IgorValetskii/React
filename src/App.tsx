5ae1854d972cf3048d7c3092d58b4e6544b5e943
Igor Valetskii <blrapok@gmail.com>
2019-10-25 00:10:04 +0300

    add todo app

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,21 @@
 import React, {Component} from 'react';
 import './App.css';
 import ShowUsers from "./users/users";
+import {TextField} from './textField';
 
-class App extends Component {
-  render(){
-    return(
-        <div>
-          <h1>Welcome to SuperLeague!</h1>
-          <button className="button">Show Users</button>
-          <ShowUsers firstName="Sadio" lastName="Mane"/>
-        </div>
-    );
-  }
-}
+const App : React.FC = () => {
+        return (
+
+            <div>
+                <TextField text='Hello' person={{firstName:'Sadio', lastName: 'Mane' }}
+                           handleChange={event => {
+
+                           }}/>
+                {/*<h1>Welcome to SuperLeague!</h1>*/}
+                {/*<button className="button">Show Users</button>*/}
+                {/*<ShowUsers firstName="Sadio" lastName="Mane"/>*/}
+            </div>
+        );
+};
 
 export default App;
