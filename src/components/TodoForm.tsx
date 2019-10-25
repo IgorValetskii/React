611cfc4e00f607cc0486ee24e80ed4fd1abb5250
IgorValetskii <blrapok@gmail.com>
2019-10-25 12:19:13 +0300

    fix clear input

--- src/components/TodoForm.tsx
+++ src/components/TodoForm.tsx
@@ -4,13 +4,14 @@ interface Props extends HTMLProps<HTMLFormElement> {
     onInputChange: (event: ChangeEvent<HTMLInputElement>) => void;
     inputValue: string;
 }
-const TodoForm = ({ onInputChange, ...props }: Props) => {
+const TodoForm = ({ onInputChange, inputValue, ...props }: Props) => {
     return (
         <form {...props}>
             <input
                 className="form-control"
                 placeholder="What would you like to get done?"
                 onChange={onInputChange}
+                value={inputValue}
             />
             <button className="btn btn-primary">Create</button>
         </form>
